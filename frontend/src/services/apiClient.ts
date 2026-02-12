cc618fbc30e33baa87a61474404653246fc81260
beginnerinvestorhub <beginnerinvestorhub@gmail.com>
2026-02-12 16:45:59 -0500

    linting fixs

--- frontend/src/services/apiClient.ts
+++ frontend/src/services/apiClient.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import { getFirebaseAuth, isFirebaseInitialized } from '@/lib/firebase';
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000';
